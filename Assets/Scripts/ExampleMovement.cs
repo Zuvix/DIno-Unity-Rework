74bb663f26091c22c54c7369511d7cac7ceb3482
sweil9 <samuelkubala@gmail.com>
2020-02-05 14:16:24 +0100

    Setup

---
+++ Assets/Scripts/ExampleMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExampleMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public float xSpeed = -2f;
+    public float ySpeed = 0f;
+
+    void FixedUpdate()
+    {
+        //Pohyb po xovej osy o 1 kazdy frame
+        transform.position = transform.position + new Vector3(xSpeed, ySpeed, 0f);
+    }
+}
---
+++ Assets/Scripts/MoveGround.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveGround : MonoBehaviour
+{
+    public float scrollSpeed = 0.8f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.x <= -15.12f)
+            transform.position = new Vector2(8.922f, transform.position.y);
+        transform.Translate(new Vector3(-scrollSpeed,0,0) *Time.deltaTime);
+    }
+}
